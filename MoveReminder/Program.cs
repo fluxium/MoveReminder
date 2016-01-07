59118e24d7adfbdd2710689408b7defcc8056b47
Mahlon Barrault <mahlonbarrault@gmail.com>
2016-01-07 16:24:07 -0700

    Inital Commit. Changed to creating event handler for SessionSwitch using a lambda so a Timer object could be passed. This stopped two separate Timer threads from activating using the previous code.

---
+++ MoveReminder/Program.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Timers;
+
+namespace MoveReminder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //SystemEvents.SessionSwitch += new SessionSwitchEventHandler(SystemEvents_SessionSwitch);
+            
+            //while (true)
+            //{
+            //    // Hold the application open
+            //}
+
+            System.Timers.Timer moveTimer = new System.Timers.Timer();
+            moveTimer.Elapsed += new ElapsedEventHandler(OnMoveElapsed);
+            moveTimer.Interval = 3600000;
+            //move.Enabled = true;
+
+            SystemEvents.SessionSwitch += (sender, e) => HandleSessionSwitch(moveTimer, sender, e);
+
+            // Prevents Windows Application from closing, keeps 
+            new System.Threading.AutoResetEvent(false).WaitOne();
+        }
+
+        static void HandleSessionSwitch(System.Timers.Timer timer, object sender, SessionSwitchEventArgs e)
+        {
+            if (e.Reason == SessionSwitchReason.SessionLock)
+            {
+                //I left my desk
+                timer.Stop();
+            }
+            else if (e.Reason == SessionSwitchReason.SessionUnlock)
+            {
+                //I returned to my desk
+                timer.Start();
+            }
+        }
+
+        //static void SystemEvents_SessionSwitch(object sender, SessionSwitchEventArgs e)
+        //{
+
+        //    if (e.Reason == SessionSwitchReason.SessionLock)
+        //    {
+        //        //I left my desk
+        //        timer.Stop();
+        //    }
+        //    else if (e.Reason == SessionSwitchReason.SessionUnlock)
+        //    {
+        //        //I returned to my desk
+        //        timer.Start();
+        //    }
+        //}
+
+        static void OnMoveElapsed(object source, ElapsedEventArgs e)
+        {
+            MessageBox.Show("Time to change your position!", "Move Reminder");
+        }
+    }
+}
